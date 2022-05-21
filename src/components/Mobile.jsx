c547b9148f5d64c732414b63662f41c0710981fa
AwsomeSwati <swati1993kushwaha@gmail.com>
2022-05-21 22:19:13 +0530

    card

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,35 @@
+
+
+import React from "react";
+
+const Mobile = () => {
+    return (
+    <div>
+        <h2>Mobile Operating System</h2>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iphone</li>
+            <li>Windows iphone</li>
+
+        </ul>
+    </div>
+    )
+};
+
+const Mobile2 = () => {
+    return (
+    <div>
+        <h2>Mobile Manufacture</h2>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+
+        </ul>
+    </div>
+    )
+};
+
+export {Mobile,Mobile2}
